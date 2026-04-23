f1202709ddd938e3157b4b2a901d592cab586e0a
Alexandr <sasha1607222@mail.ru>
2026-04-23 21:18:47 +0300

    feat: implement MVP class architecture for sprint 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "DepositorRepository.h"
+#include "UserInterface.h"
 
 int main() {
-    std::cout << "Sberbank Depositors Management System. Alpha v0.0.1" << std::endl;
+    // Устанавливаем русскую локаль для корректного вывода в консоль
+    setlocale(LC_ALL, "Russian");
+
+    DepositorRepository repo;
+    UserInterface ui(repo);
+    ui.run();
+
     return 0;
 }
+
